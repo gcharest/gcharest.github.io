dc6b988fea0031c6b4dbbd33dd6086bf07b52f84
Guillaume Charest <guillaume.w.charest@gmail.com>
2020-02-20 23:31:21 -0500

    Remove grouping and sort by col 0 by default

--- assets/js/tableFoodFactors.js
+++ assets/js/tableFoodFactors.js
@@ -4,33 +4,13 @@ $(document).ready(function () {
     var pageLang = document.getElementsByTagName("html")[0].getAttribute('lang');
     var parametersValues = {
       "columnDefs": [{
-        "visible": false,
-        "targets": groupColumn
+        "visible": false
       }],
       "order": [
-        [groupColumn, 'asc']
+        [3, 'asc']
       ],
       "displayLength": 25,
-      "scrollX": true,
-      "drawCallback": function (settings) {
-        var api = this.api();
-        var rows = api.rows({
-          page: 'current'
-        }).nodes();
-        var last = null;
-
-        api.column(groupColumn, {
-          page: 'current'
-        }).data().each(function (group, i) {
-          if (last !== group) {
-            $(rows).eq(i).before(
-              '<tr class="group"><td colspan="5">' + group + '</td></tr>'
-            );
-
-            last = group;
-          }
-        });
-      }
+      "scrollX": true
     }
     if (pageLang == 'fr') {
       parametersValues.language = {
@@ -67,16 +47,6 @@ $(document).ready(function () {
     return parametersValues;
   };
   
-  var groupColumn = 3;
-  var table = $('#tableFoodFactors').DataTable(parameters());
-
-  // Order by the grouping
-  $('#example tbody').on('click', 'tr.group', function () {
-    var currentOrder = table.order()[0];
-    if (currentOrder[0] === groupColumn && currentOrder[1] === 'asc') {
-      table.order([groupColumn, 'desc']).draw();
-    } else {
-      table.order([groupColumn, 'asc']).draw();
-    }
-  });
+  $('#tableFoodFactors').DataTable(parameters())
+  
 });
