2fda5edccf1a928b30d3efb389a08664eb17671d
Guillaume <guillaume.charest@cds-snc.ca>
2026-03-21 23:34:18 -0400

    feat: add DMARC, SPF, and null MX record support

--- terragrunt/modules/route53-records/main.tf
+++ terragrunt/modules/route53-records/main.tf
@@ -1,3 +1,16 @@
+locals {
+  dmarc_tags = compact([
+    "v=DMARC1",
+    "p=${var.dmarc_policy}",
+    "pct=${var.dmarc_pct}",
+    var.dmarc_subdomain_policy == null ? null : "sp=${var.dmarc_subdomain_policy}",
+    var.dmarc_aggregate_report_uri == null ? null : "rua=mailto:${var.dmarc_aggregate_report_uri}",
+    var.dmarc_forensic_report_uri == null ? null : "ruf=mailto:${var.dmarc_forensic_report_uri}",
+    var.dmarc_alignment_dkim == null ? null : "adkim=${var.dmarc_alignment_dkim}",
+    var.dmarc_alignment_spf == null ? null : "aspf=${var.dmarc_alignment_spf}",
+  ])
+  dmarc_record = join("; ", local.dmarc_tags)
+}
 resource "aws_route53_record" "cloudfront" {
   zone_id = var.route53_zone_id
   name    = var.domain_name
@@ -46,4 +59,35 @@ resource "aws_route53_record" "www_ipv6" {
     zone_id                = var.cloudfront_zone_id
     evaluate_target_health = false
   }
+}
+
+
+resource "aws_route53_record" "spf" {
+  count   = var.enable_spf_record ? 1 : 0
+  zone_id = var.route53_zone_id
+  name    = var.domain_name
+  type    = "TXT"
+  ttl     = var.txt_ttl
+
+  records = [var.spf_record_value]
+}
+
+resource "aws_route53_record" "null_mx" {
+  count   = var.enable_null_mx_record ? 1 : 0
+  zone_id = var.route53_zone_id
+  name    = var.domain_name
+  type    = "MX"
+  ttl     = var.txt_ttl
+
+  records = [var.null_mx_record_value]
+}
+
+resource "aws_route53_record" "dmarc" {
+  count   = var.enable_dmarc_record ? 1 : 0
+  zone_id = var.route53_zone_id
+  name    = "_dmarc.${var.domain_name}"
+  type    = "TXT"
+  ttl     = var.txt_ttl
+
+  records = [local.dmarc_record]
 }
